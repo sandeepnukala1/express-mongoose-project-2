fe70341a5af0d5df5bd99684ab7a8a61d5f990d5
Sandeep <sandeepn@bu.edu>
2021-04-18 14:18:59 -0400

    Role based access to deleting resource

--- routes/resources.js
+++ routes/resources.js
@@ -15,7 +15,13 @@ router.use(AuthorizationCtrl.addUserToRequest)
 // Router Routes
 ////////////////////////////////
 router.get("/", AuthorizationCtrl.isAuthorized, async (req, res) => {
-    const resources = await User.find({})
+    const resources = await User.find({}).lean().exec()
+    let newResources = []
+    newResources = resources.map(r => {
+        r.loggedInRole = req.user.role
+        return r
+    })
+    console.log(newResources)
     res.render("resources/resources", { resources })
 })
 
@@ -35,6 +41,27 @@ router.post("/new", AuthorizationCtrl.isAuthorized, async (req, res) => {
             res.redirect("/resources")
         }
     })
+})
+
+router.delete("/:id", AuthorizationCtrl.isAuthorized, async (req,res) => {
+    const id = req.params.id
+    await User.findByIdAndDelete(id)
+    res.redirect("/resources")
+})
+
+router.put("/:id", AuthorizationCtrl.isAuthorized, async (req,res) => {
+    const id = req.params.id
+    const salt = await bcrypt.genSalt(10)
+    // hash the password
+    req.body.password = await bcrypt.hash(req.body.password, salt)
+    await User.findByIdAndUpdate(id, req.body, {new: true})
+    res.redirect("/resources")
+})
+
+router.get("/:id", AuthorizationCtrl.isAuthorized, async (req,res) => {
+    const id = req.params.id
+    const resource = await User.findById(id)
+    res.render("resources/showResource", { resource })
 })
 
 module.exports = router
